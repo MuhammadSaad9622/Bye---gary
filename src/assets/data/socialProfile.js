084c03863068288dd53af161cd921c7b9402e52a
MuhammadSaad9622 <saadnadeem962@gmail.com>
2024-02-17 18:50:52 +0500

    .

---
+++ src/assets/data/socialProfile.js
@@ -0,0 +1,34 @@
+import {
+  FaLinkedinIn,
+  FaTwitter,
+  FaInstagram,
+  FaTelegramPlane,
+  FaFacebook,
+  FaDiscord
+} from "react-icons/fa";
+
+import openseaIcon from "../images/icon/opensea.svg";
+import mediumIcon from "../images/icon/med.svg";
+
+const data = [
+
+  {
+    icon: <FaTwitter />,
+    url: "https://twitter.com/ByeGaryNFT?t=8v1TEKRZriGIVltbeP-XIw&s=09&fbclid=IwAR0Yq3C_iPYROMTIXpBSAoKg7G72sxMaPV0hsGabfUgZ3A4xcIIGpe-DwjM",
+  },
+  {
+    icon: <FaDiscord />,
+    url: "https://discord.gg/4hWVdgdf",
+  },
+  // {
+  //   icon: <FaInstagram />,
+  //   url: "@hiscollection.co",
+  // },
+
+  // {
+  //   thumb: mediumIcon,
+  //   url: "#",
+  // },
+];
+
+export default data;
